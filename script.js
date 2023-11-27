c79a39b410b6a43840d8220ea396790ac7447665
PehoD <dominik.peho98@gmail.com>
2023-11-27 23:00:52 +0100

    Tabbed component

--- script.js
+++ script.js
@@ -64,6 +64,29 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 /// SELECTING ELEMENTS --------------------
@@ -157,4 +180,28 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
 });
 randomColor(0, 255);
+*/
+/*
+// Dom traversing
+
+const h1 = document.querySelector('h1');
+
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parent
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// Going sideways: siblings
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
 */
